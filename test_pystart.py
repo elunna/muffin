dcfdb579b2ec640f379168f7f52b077e3b63e01e
Erik Lunna <eslunna@gmail.com>
2016-11-01 11:14:42 -0400

    Removed the extra cleanup call from all the setupdirs tests, it was redundant.

--- test_pystart.py
+++ test_pystart.py
@@ -23,32 +23,27 @@ def cleanup():
 def test_setupdirs_makes_project_dir():
     pystart.setup_dirs(TEST_PROJ)
     assert os.path.isdir(TEST_PROJ)  # Error making project dir
-    cleanup()
 
 
 def test_setupdirs_src_dir():
     pystart.setup_dirs(TEST_PROJ)
     srcdir = TEST_PROJ + 'src'  # Error making project/src dir
     assert os.path.isdir(srcdir)
-    cleanup()
 
 
 def test_setupdirs_data_dir():
     pystart.setup_dirs(TEST_PROJ)
     srcdir = TEST_PROJ + 'data'
     assert os.path.isdir(srcdir)  # Error making project/data dir
-    cleanup()
 
 
 def test_setupdirs_tests_dir():
     pystart.setup_dirs(TEST_PROJ)
     srcdir = TEST_PROJ + 'tests'
     assert os.path.isdir(srcdir)  # Error making project/tests dir
-    cleanup()
 
 
 def test_setupdirs_temp_dir():
     pystart.setup_dirs(TEST_PROJ)
     srcdir = TEST_PROJ + 'temp'
     assert os.path.isdir(srcdir)  # Error making project/temp dir
-    cleanup()
