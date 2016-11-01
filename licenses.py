a8fe9fc1ff59d31ff5840b739e6ffbff055f0b4f
Erik Lunna <eslunna@gmail.com>
2016-11-01 14:33:47 -0400

    Added GNU ver 2 and 3 docs, and the brief license info to licenses.py.

--- licenses.py
+++ licenses.py
@@ -23,3 +23,34 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+
+GNU = """
+GNU GENERAL PUBLIC LICENSE
+Version 3, 29 June 2007
+
+one line to give the program's name and an idea of what it does.
+Copyright (C) {yyyy}  {name}
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+
+
+GNU_INTERACTIVE = """
+Gnomovision version 69, Copyright (C) {year} {name}
+Gnomovision comes with ABSOLUTELY NO WARRANTY; for details
+type `show w'.  This is free software, and you are welcome
+to redistribute it under certain conditions; type `show c'
+for details.
+"""
