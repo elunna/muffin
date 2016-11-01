54b3c2fa3b8b494aaf5b4047713902981138d5b3
Erik Lunna <eslunna@gmail.com>
2016-11-01 10:07:18 -0400

    First commit: Outline of project in README and outline of functions in pystart.py

---
+++ README.md
@@ -0,0 +1,42 @@
+Name: Professional Python Project starter
+
+usage: pystart projectname
+
+Objectives:
+    * Setup the directory structure 
+        /project/
+            /src
+                __init__.py
+            /tests
+                __init__.py
+            /logs
+            /data
+            /temp
+
+    * Setup the virtualenv
+        * Use Python 2(later option to use python3)
+        * Initialize a git repo
+
+    * Write startup files:
+        - initial README.md file.
+        - starting tests to verify the packages work.
+        - requirements.txt
+        - .gitignore
+
+    * Use a wizard to make things nice and customizable.
+        - Project name
+        - Author - Default in my settings
+        - Start Date - default is now
+        - End Date
+        - Purpose
+        - Description
+        - License
+        - modules: Here we can list what starting modules we can outline, each will have a corresponding test file.
+
+
+    * Main.py run file.
+        * logger, and  config file
+        * argparse
+        * main method
+
+
---
+++ pystart.py
@@ -0,0 +1,69 @@
+import os
+
+
+def setup_dir_structure():
+    # Create main project directory
+    # Create main/src
+    # Create main/tests
+    # Create main/data
+    # Create main/temp
+    pass
+
+
+def setup_virtualenv():
+    # Setup python 2 virtualenv
+    # Upgrade pip
+    # Install needed packages:
+    #   py.test
+    #
+    # Setup an autoenv file???
+    # requirements.txt
+    pass
+
+
+def setup_git():
+    # Initialize git repo
+    # Create thorough .gitignore
+    pass
+
+
+# Create a main.py run file
+def setup_main():
+    # Setup logger
+    # Setup argparse, basic.
+    # Create main run func
+    pass
+
+def get_date():
+    # Returns todays date
+    pass
+
+def setup_README():
+    # Add info from wizard
+    # Add licence info
+    pass
+
+
+def check_existing():
+    # Checks if the project folder exists already
+    pass
+
+
+def wizard():
+    # Ask for author
+    # Ask for project name
+    # Ask for project purpose
+    # Ask for Start date
+    # Ask for end date
+    # Create functional tests for
+        # py-test
+        # logger
+        #
+    # Ask for starting modules
+        # For each module, ask for starting functions and create tests for each
+
+    # Ask if we want Beautiful Soup and request
+        # Functional test
+    # Ask if we want scrapy
+        # Functional test
+    pass
