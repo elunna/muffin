7b653b8deb2b7b9dbacfa22f78c849ffc8070ba2
Erik Lunna <eslunna@gmail.com>
2016-11-01 10:51:08 -0400

    Added failing test for creation of the src dir.

--- test_pystart.py
+++ test_pystart.py
@@ -1,13 +1,16 @@
 import os
 import shutil
 import pystart
+import pytest
 """
 Tests for setup_dirs()
 """
 TEST_PROJ = 'testproject/'
 
 
+@pytest.yield_fixture(autouse=True)
 def cleanup():
+    yield None
     if os.path.isdir(TEST_PROJ):
         shutil.rmtree(TEST_PROJ)
 
@@ -15,4 +18,11 @@ def cleanup():
 def test_setupdirs_makes_project_dir():
     pystart.setup_dirs(TEST_PROJ)
     assert os.path.isdir(TEST_PROJ)
+    cleanup()
+
+
+def test_setupdirs_src_dir():
+    pystart.setup_dirs(TEST_PROJ)
+    srcdir = TEST_PROJ + 'src'
+    assert os.path.isdir(srcdir)
     cleanup()
