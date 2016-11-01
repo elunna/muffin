47e0414ee3178271dc90d4e5a7dc7b27ee72ea93
Erik Lunna <eslunna@gmail.com>
2016-11-01 14:09:55 -0400

    Adde failing test to check for Purpose field in README.

--- test_pystart.py
+++ test_pystart.py
@@ -80,6 +80,13 @@ def test_makereadme_projectname():
         assert t in f.read().splitlines()  # Check if Project Name field is in README
 
 
+def test_makereadme_purpose():
+    readme = readme_factory()
+    t = "Purpose: Test"
+    with open(readme, 'r') as f:
+        assert t in f.read().splitlines()  # Check if Purpose field is in README
+
+
 def test_makereadme_startdate():
     readme = readme_factory()
     t = "Start Date: 2016-01-01"
