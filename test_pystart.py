3024d244a4f81ad78885bc4abe4a2b75bf0b9e3d
Erik Lunna <eslunna@gmail.com>
2016-11-01 11:09:02 -0400

    Added failing test for making the data dir. Also added comments to tests so we can see why things are failing.

--- test_pystart.py
+++ test_pystart.py
@@ -22,12 +22,19 @@ def cleanup():
 
 def test_setupdirs_makes_project_dir():
     pystart.setup_dirs(TEST_PROJ)
-    assert os.path.isdir(TEST_PROJ)
+    assert os.path.isdir(TEST_PROJ)  # Error making project dir
     cleanup()
 
 
 def test_setupdirs_src_dir():
     pystart.setup_dirs(TEST_PROJ)
-    srcdir = TEST_PROJ + 'src'
+    srcdir = TEST_PROJ + 'src'  # Error making project/src dir
     assert os.path.isdir(srcdir)
+    cleanup()
+
+
+def test_setupdirs_data_dir():
+    pystart.setup_dirs(TEST_PROJ)
+    srcdir = TEST_PROJ + 'data'
+    assert os.path.isdir(srcdir)  # Error making project/data dir
     cleanup()
