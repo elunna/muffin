40d58fc078755352257dca320992aed3f3b90fe1
Erik Lunna <eslunna@gmail.com>
2016-11-01 10:28:48 -0400

    Added a cleanup function to test_pystart to remove the test project after testing.

--- test_pystart.py
+++ test_pystart.py
@@ -1,11 +1,18 @@
 import os
+import shutil
 import pystart
 """
 Tests for setup_dirs()
 """
+TEST_PROJ = 'testproject/'
+
+
+def cleanup():
+    if os.path.isdir(TEST_PROJ):
+        shutil.rmtree(TEST_PROJ)
 
 
 def test_setupdirs_makes_project_dir():
-    p = 'testproject'
-    pystart.setup_dirs(p)
-    assert os.path.isdir(p)
+    pystart.setup_dirs(TEST_PROJ)
+    assert os.path.isdir(TEST_PROJ)
+    cleanup()
