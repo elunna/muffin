29fa745af98e7efacd533e5d34de0f489ce92a13
Erik Lunna <eslunna@gmail.com>
2016-11-02 01:48:05 -0400

    Added 3 tests for valid_license.

--- test_wizard.py
+++ test_wizard.py
@@ -21,3 +21,20 @@ def test_valid_projectname_spacesonends():
     t = ' new_project '
     expected = True
     assert wizard.valid_projectname(t) == expected
+
+
+"""
+Tests for valid_license(license)
+"""
+
+
+def test_validlicense_MIT():
+    assert wizard.valid_license('mit')
+
+
+def test_validlicense_GNU():
+    assert wizard.valid_license('GNU')
+
+
+def test_validlicense_XXX():
+    assert wizard.valid_license('XXX') is False
