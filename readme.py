1a8b8de58908f4478eed48e27c138a8ac45ecc8e
Erik Lunna <eslunna@gmail.com>
2016-11-01 22:26:37 -0400

    Added a markdown template to readme.py

---
+++ readme.py
@@ -0,0 +1,51 @@
+template = """
+<h2 name="top">What's it?</h2>
+:question:
+
+[Project summary]
+
+[comment]: <> (![](screenshot.png))
+
+### Features
+- **Cool thing** Super Acme 3000 batteries included
+- **Stupendis thing!** (HD Full-screen)
+
+---
+
+### Prerequisities
+Python 2
+
+### Installing
+Linux:
+Max OS X: n/a
+Windows: n/a
+
+---
+
+## Usage :horse
+```
+$ python main.py
+```
+For help: python main.py -h
+
+***EXAMPLES:***
+> Example 1:
+```
+$ python main.py 1
+```
+
+> Example 2:
+```
+$ python main.py 2
+```
+
+---
+
+### TODO
+-----------------
+- [ ] Finish README :sparkles:
+- [ ] Create first functional test
+
+---
+
+"""
