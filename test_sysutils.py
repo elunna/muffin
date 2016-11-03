7fd4d64f794572adeca8dbf6f9c510997b5c7294
Erik Lunna <eslunna@gmail.com>
2016-11-03 03:02:05 -0400

    Add sysutils tests for: cmd_result, chk_sys_for, chk_pip_for, and chk_python.

--- test_sysutils.py
+++ test_sysutils.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 import sysutils
 
 """
@@ -20,15 +21,60 @@ def test_cmdresult_touch():
 """
 Tests for chk_sys_for(app)
 """
-# chk_sys_for('weirdo')
+
+
+def test_chksysfor_nonexistant():
+    cmd = 'weirdo'
+    expected = False
+    assert sysutils.chk_sys_for(cmd) == expected
+
+
+def test_chksysfor_ls():
+    # Every linux system has ls
+    cmd = 'ls'
+    expected = True
+    assert sysutils.chk_sys_for(cmd) == expected
+
 
 """
 Tests for chk_pip_for(lib)
 """
 
+
+def test_chkpipfor_nonexistant():
+    cmd = 'weirdo'
+    expected = False
+    assert sysutils.chk_pip_for(cmd) == expected
+
+
+def test_chkpipfor_pip():
+    # If we have pip, it should almost certainly show it!
+    cmd = 'pip'
+    expected = True
+    assert sysutils.chk_pip_for(cmd) == expected
+
+
 """
 Tests for chk_python()
 """
+py2version = pytest.mark.skipif(sysutils.chk_sys_for('python2') is False,
+                                reason="python2 required")
+
+
+@py2version
+def test_chkpython_2():
+    result = sysutils.chk_python()
+    assert '2.7' in result
+
+py3version = pytest.mark.skipif(sysutils.chk_sys_for('python3') is False,
+                                reason="python3 required")
+
+
+@py3version
+def test_chkpython_3():
+    result = sysutils.chk_python()
+    assert '2.7' in result
+
 
 """
 Tests for new_virtualenv(py_version)
