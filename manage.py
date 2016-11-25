cacacd5c64f12ea0d67b4b90665e29399d241cb6
Erik Lunna <eslunna@gmail.com>
2016-11-25 14:34:25 -0500

    Pylinted the hell out of manage.py

--- manage.py
+++ manage.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 """
-Inspired by the Django manage - a tool to manage mundane project tasks.
-"""
+  " Inspired by the Django manage - a tool to manage mundane project tasks.
+  """
 import argparse
 import json
 import subprocess
 
 
 def clean():
-    """Cleans out non-essential project files:
+    """ Cleans out non-essential project files:
         From directories: /data, /temp, /logs directories
         Removes .ropeproject, __pycache__, .cache directories
         Removes all *.pyc files.
@@ -29,32 +29,27 @@ def clean():
 
 
 def archive():
-    """
-    Zip up the /data directory, and optionally the logs directory
-    """
+    """ Zip up the /data directory, and optionally the logs directory """
 
 
 def newmod():
-    """
-    Create basic templates for a new module and test
-    """
+    """ Create basic templates for a new module and test """
 
 
 def freeze():
-    """
-    Create or replace the requirements.txt from pip.
-    """
+    """ Create or replace the requirements.txt from pip.  """
     cmd = 'pip freeze > requirements.txt'
     subprocess.call(cmd, shell=True)
 
 
-def setup_sphinx(config):
+def setup_sphinx(conf):
+    """ Sets up the Sphinx documentation with as many defaults as possible. """
     cmd = [
         'sphinx-quickstart',
         '--sep',
         '--dot=_',
-        '-p', config.get('projectname', ''),
-        '-a', config.get('author', ''),
+        '-p', conf.get('projectname', ''),
+        '-a', conf.get('author', ''),
         '-v', '1.0',
         '-r', '1.0',
         '-l', 'en',
@@ -73,6 +68,7 @@ def setup_sphinx(config):
 
 
 def get_defaults():
+    """ Retrieves the settings the user entered during the project setup wizard."""
     with open('config.json', 'r') as f:
         contents = f.read()
         defaults = json.loads(contents)
@@ -80,7 +76,8 @@ def get_defaults():
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Helper utility for managing mundane project tasks.")
+    parser = argparse.ArgumentParser(
+        description="Helper utility for managing mundane project tasks.")
 
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-c', '--clean', action='store_true',
